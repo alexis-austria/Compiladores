dcfc462289e60d41a6144660c324c3536fb3797a
Pablo Candela <pabloantonio@ciencias.unam.mx>
2018-10-25 20:55:51 -0500

    Update NodoWhile.java

--- Proyectos/Proyecto3/src/main/java/ast/patron/compuesto/NodoWhile.java
+++ Proyectos/Proyecto3/src/main/java/ast/patron/compuesto/NodoWhile.java
@@ -4,7 +4,7 @@ import ast.patron.visitante.*;
 Public class NodoWhile extends compuesto{
 
     Public NodoWhile(Nodo l, Nodo r){
-        this.agregaHijoPrint(l);
+        this.agregaHijoPrincipio(l);
         this.agregaHijoFinal(r);
     }
 
