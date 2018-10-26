95b12392316c15b379992eb8dfe7399be0321b47
Pablo Candela <pabloantonio@ciencias.unam.mx>
2018-10-25 19:41:00 -0500

    Update VisitorPrint.java

--- Proyectos/Proyecto3/src/main/java/ast/patron/visitante/VisitorPrint.java
+++ Proyectos/Proyecto3/src/main/java/ast/patron/visitante/VisitorPrint.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 
 public class VisitorPrint implements Visitor
 {
-
+    //Hojas
     public void visit(IntHoja n){
 	System.out.print("[Hoja Entera] valor: " + n.getValor().ival);
     }
@@ -25,15 +25,134 @@ public class VisitorPrint implements Visitor
     public void visit(IdentificadorHoja s){
 	System.out.print("[Hoja Identificador] id: " + s.getNombre());
     }
-
+    
+    //Nodos
     public void visit(Nodo n){
 
     }
+    
+    public void visit(NodoGr n){
+        System.out.println("GR");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }   
 
-    public void visit(NodoStmt n) {
+    public void visit(NodoGrq n){
+        System.out.println("GRQ");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }  
 
+    public void visit(NodoLe n){
+        System.out.println("LE");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }    
+    
+    public void visit(NodoLeq n){
+        System.out.println("LEQ");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }    
+
+    public void visit(NodoMas n){
+        System.out.println("MAS");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
     }
 
+    public void visit(NodoModulo n){
+        System.out.println("MODULO");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    } 
+
+    public void visit(NodoNot n){
+        System.out.println("NOT");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }        
+
+    public void visit(NodoOr n){
+        System.out.println("OR");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }   
+
+    public void visit(NodoPor n){
+        System.out.println("POR");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }   
+
+
+    public void visit(NodoPotencia n){
+        System.out.println("POTENCIA");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }
+
+    public void visit(NodoPrint n){
+        System.out.println("PRINT");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+    } 
+
+    public void visit(NodoWhile n){
+        System.out.println("WHILE");
+        System.out.print("(");
+        n.getPrimerHijo().accept(this);
+        System.out.print(")");
+        System.out.print("(");
+        n.getUltimoHijo().accept(this);
+        System.out.println(")");
+    }     
+
+    public void visit(NodoStmts n){
+    }   
+
+   
     public void visit(NodoSimpStmt n) {
 
     }
