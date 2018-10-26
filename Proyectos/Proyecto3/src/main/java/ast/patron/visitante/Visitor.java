073f5174238842ef904d891bc8945250f722154c
Pablo Candela <pabloantonio@ciencias.unam.mx>
2018-10-25 19:37:13 -0500

    Update Visitor.java

--- Proyectos/Proyecto3/src/main/java/ast/patron/visitante/Visitor.java
+++ Proyectos/Proyecto3/src/main/java/ast/patron/visitante/Visitor.java
@@ -3,14 +3,26 @@ import ast.patron.compuesto.*;
 
 public interface Visitor
 {
+    //Hojas
     public void visit(IntHoja n);
     public void visit(BooleanoHoja b);
     public void visit(RealHoja f);
     public void visit(CadenaHoja s);
     public void visit(IdentificadorHoja s); 
     public void visit(Nodo n);
-    public void visit(IntHoja n);
-    public void visit(Nodo n);
+    //Nodos
+    public void visit(NodoGr n);
+    public void visit(NodoGrq n);
+    public void visit(NodoLe n);
+    public void visit(NodoLeq n);
+    public void visit(NodoMas n);
+    public void visit(NodoModulo n);
+    public void visit(NodoNot n);
+    public void visit(NodoOr n);
+    public void visit(NodoPor n);
+    public void visit(NodoPotencia n);
+    public void visit(NodoPrint n);
+    public void visit(NodoWhile n);
     public void visit(NodoStmt n);
     public void visit(NodoSimpStmt n);
     public void visit(NodoCompStmt n);
